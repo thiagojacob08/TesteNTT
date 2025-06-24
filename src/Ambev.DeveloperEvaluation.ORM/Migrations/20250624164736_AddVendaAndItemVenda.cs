326053616f5d6c3343a411b5357590a11ccbf9a6
thiago jacob <thiagojacob34@hotmail.com>
2025-06-24 14:22:34 -0300

    Dia 03

---
+++ src/Ambev.DeveloperEvaluation.ORM/Migrations/20250624164736_AddVendaAndItemVenda.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Ambev.DeveloperEvaluation.ORM.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVendaAndItemVenda : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Users",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Vendas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    DataVenda = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Cliente = table.Column<string>(type: "text", nullable: false),
+                    Filial = table.Column<string>(type: "text", nullable: false),
+                    Cancelado = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vendas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItensVenda",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Produto = table.Column<string>(type: "text", nullable: false),
+                    Quantidade = table.Column<int>(type: "integer", nullable: false),
+                    PrecoUnitario = table.Column<decimal>(type: "numeric", nullable: false),
+                    Desconto = table.Column<decimal>(type: "numeric", nullable: false),
+                    VendaId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItensVenda", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ItensVenda_Vendas_VendaId",
+                        column: x => x.VendaId,
+                        principalTable: "Vendas",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItensVenda_VendaId",
+                table: "ItensVenda",
+                column: "VendaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ItensVenda");
+
+            migrationBuilder.DropTable(
+                name: "Vendas");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Users");
+        }
+    }
+}
